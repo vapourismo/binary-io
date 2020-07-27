e5b9dbc23195d34fabdd02f5d4ee3be950e83075
Ole Krüger <ole@vprsm.de>
2020-07-27 17:28:51 +0100

    Move shared single-Word8 buffer to global scope
    This allows us to avoid the allocation which happens upon every creation
    of a pair of Await and Notify.

--- lib/Data/Binary/IO/Internal/AwaitNotify.hs
+++ lib/Data/Binary/IO/Internal/AwaitNotify.hs
@@ -9,11 +9,17 @@ module Data.Binary.IO.Internal.AwaitNotify
   )
 where
 
-import qualified Foreign
-
 import           Data.Word (Word8)
+import qualified Foreign
 import qualified System.IO as IO
+import           System.IO.Unsafe (unsafePerformIO)
 import qualified System.Process as Process
+
+-- | Static pointer that points to a single 'Word8'
+someWord8Ptr :: Foreign.Ptr Word8
+someWord8Ptr = unsafePerformIO (Foreign.calloc @Word8)
+
+{-# NOINLINE someWord8Ptr #-}
 
 -- | Await signal from a paired 'Notify'. Returns 'False' if the paired 'Notify' does not exist
 -- (any more).
@@ -24,9 +30,9 @@ newtype Await = Await
 newtype Notify = Notify
   { runNotify :: IO () }
 
+-- | Create a pair of 'Await' and 'Notify.
 newAwaitNotify :: IO (Await, Notify)
 newAwaitNotify = do
-  buf <- Foreign.calloc @Word8
   (read, write) <- Process.createPipe
 
   IO.hSetBuffering read IO.NoBuffering
@@ -35,7 +41,7 @@ newAwaitNotify = do
   IO.hSetBinaryMode read True
   IO.hSetBinaryMode write True
 
-  let notify = IO.hPutBuf write buf 1
-  let await  = (> 0) <$> IO.hGetBufSome read buf 1
+  let notify = IO.hPutBuf write someWord8Ptr 1
+  let await  = (> 0) <$> IO.hGetBufSome read someWord8Ptr 1
 
   pure (Await await, Notify notify)
